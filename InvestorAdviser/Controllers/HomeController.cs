93ad1da39d3c73cc26a88346056b2f1ff7ea454a
juanjo <juanjosesierramonton@live.com>
2015-10-02 21:59:18 +0200

    test commit

--- InvestorAdviser/Controllers/HomeController.cs
+++ InvestorAdviser/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace InvestorAdviser.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View();//
         }
 
         public ActionResult About()
